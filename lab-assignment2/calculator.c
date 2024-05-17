d91b400380a5340b2b9cae0e37585a317e8423d2
Jonathan Mowat <bobbie@Jonathans-MacBook-Air.local>
2024-05-17 11:28:30 -0700

    upload Lab 2 assignment

--- lab-assignment2/calculator.c
+++ lab-assignment2/calculator.c
@@ -1,18 +1,48 @@
-//enter your name here
-//enter your email here
+// name: Jonathan Mowat
+// email: mowat.j@northeastern.edu
+
 #include<stdio.h>
 int main()
 {
     int a,b,c, choice;
     
     printf("Enter your choice\n");
-    printf(" 1.addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n");
+    printf(" 1. Addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n");
     scanf("%d",&choice);
     
-    printf("Enter a and b values");
+    printf("Enter a and b values ");
     scanf("%d %d", &a, &b);
+
+    printf("Inputs: %d %d \n", a, b);
     
     // Add your code here using switch
+    switch (choice)
+    {
+    case 1: 
+        printf("Addition\nSum = %d\n", (a+b));
+        break;
+    case 2:
+        printf("Subtraction\nDifference = %d\n", (a-b));
+        break;
+    case 3:
+        printf("Multiplication\nProduct = %d\n", (a*b));
+        break;
+    case 4:
+        if (b == 0){
+            printf("Divide by zero error\n");
+            break;
+        }
+        else{
+            printf("Division\nQuotient = %d\n", (a/b));
+            if(a%b != 0){
+                printf("Remainder = %d/%d\n", a%b, b);
+            }
+        }
+        break;
+    default:
+        printf("Invalid Option\n");
+        break;
+    }
            
     return 0;
 }
--- lab-assignment2/employee.c
+++ lab-assignment2/employee.c
@@ -14,12 +14,26 @@ typedef struct employee
 void readData(int n, employee_t* s)
 {
     // Add your code here
+     employee_t* thisEmployee;
+    for (int i = 0; i<n; i++) {
+        thisEmployee = &s[i];
+        printf("Enter the employeeID, Name, Designation and Department of Employee %d:\n",i+1);
+        scanf("%d %s %s %s", &(thisEmployee->empId),  thisEmployee->Name, thisEmployee->Designation, thisEmployee->Dept);
+
+    }
 }
 /* Function to print the employee details*/
 void display(int n, employee_t * s)
 {
     // Add your code here
-    
+    employee_t* thisEmployee;
+    for (int i = 0; i<n; i++) {
+        thisEmployee = &s[i];
+        printf("The details of the Employee %d:\n"
+                " empId: %d\n Name: %s\n Designation: %s\n Dept: %s\n",
+                i+1 ,thisEmployee->empId,  thisEmployee->Name, thisEmployee->Designation, thisEmployee->Dept);
+
+    }
 }
 
 /*----------Main Program--------*/
@@ -27,8 +41,23 @@ int main()
 
 {
     // Main Function to print the employee details
-    // Add your code here
+    int n;
     
+    printf("Enter the number of employees: ");
+    scanf("%d",&n);
+
+    // emp is a pointer to a list of the pointers for each employee
+    employee_t *emp = (employee_t *)malloc(n * sizeof(employee_t));
+
+    
+    readData(n, emp);
+    display(n, emp);
+
+
+    free(emp);
+
+
+     
 
 }
 
