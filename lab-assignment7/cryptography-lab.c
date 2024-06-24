012a2d1e848bdf26d6ed9753f8da108bd25ea5f9
Jonathan Mowat <bobbie@Jonathans-MacBook-Air.local>
2024-06-23 17:10:46 -0700

    upload lab7

--- lab-assignment7/cryptography-lab.c
+++ lab-assignment7/cryptography-lab.c
@@ -1,5 +1,5 @@
-//enter your name here
-//enter your email here
+// name: Jonathan Mowat
+// email: mowat.j@northeastern.edu
 
 #include <stdio.h>
 #include <string.h>
@@ -8,7 +8,18 @@
 /* function to encrypt the data*/
 void encrypt(char text[], int key)
 {
+    
     // Add your code here
+    for (int i = 0; i<strlen(text); i++) {
+        char x = text[i];
+        if (x >= 65 && x <= 90){ // Uppercase A-Z
+            x = (x - 'A' + key) % 26 + 'A';
+            text[i] = x;
+        } else if (x >= 97 && x <= 122) { // lowercase a-z
+            x = (x - 'a' + key) % 26 + 'a';
+            text[i] = x;
+        }
+    }
 }
 
 /*function to decrypt the data*/
@@ -16,13 +27,23 @@ void decrypt(char text[],int key)
 {
     
     // Add your code here
-    
+    for (int i = 0; i< strlen(text); i++){
+        char x = text[i];
+        if (x >= 65 && x <= 90){ // Uppercase A-Z
+            x = (x - 'A' - key + 26) % 26 + 'A';
+            text[i] = x;
+        } else if (x >= 97 && x <= 122) { // lowercase a-z
+            x = (x - 'a' - key + 26) % 26 + 'a';
+            text[i] = x;
+        } 
+    }
 }
 
 
 /*----------- Main program---------------*/
 int main()
 {
+    
     char text[20] ;
     int keyvalue=3;
     /*Input string */
@@ -37,6 +58,7 @@ int main()
     /*call function to decrypt the data*/
     decrypt(text,keyvalue);
     printf("Decrypted value = %s\n",text);
+
     
     return 0;
 }
